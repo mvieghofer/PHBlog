10a804f7a922831f7c7ac4c170648f41df8f59b2
Markus Vieghofer <markus.vieghofer@gmail.com>
2015-05-01 20:33:08 +0200

    added markdown support for edit page. All changes will be displayed in a preview window immediately.

--- edit.php
+++ edit.php
@@ -13,7 +13,7 @@
         } else {
             Article::insert($article);    
         }
-        header("Location: edit.php?pageId=$article->id");
+        header("Location: dashboard.php");
     }
     
     if (isset($_GET["pageId"])) {
@@ -33,22 +33,63 @@
             echo "New";
         }
     ?></title>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="libs/markdown/lib/markdown.js"></script>
+    
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
 <body>
-    <form action="edit.php<?php if (isset($_GET["pageId"])) { echo '?pageId='.$_GET["pageId"]; } ?>" method="post">
+    <form action="edit.php<?php if (isset($_GET["pageId"])) { echo '?pageId='.$_GET["pageId"]; } ?>" method="post" id="editForm">
         <input type="hidden" value="<?php if (!isset($_GET["pageId"])) { echo 'newpost'; } else { echo 'edit'; } ?>" name="mode">
         <?php
             if (isset($_GET["pageId"])) {
                 echo "<input type='hidden' name='pageId' value='" . $_GET["pageId"] . "'>";
             }
         ?>
-        <input name="headline" value=" <?php echo $article->headline ?> " /><br />
-        <input type="checkbox" name="ispage" <?php if ($article->ispage) { echo 'checked'; } ?>>Is Page</input><br />
-        <textarea name="content" value="" cols="150" rows="50"><?php echo $article->content ?></textarea><br />
-        <button type="submit">Submit</button>
-    </form>
+        <input name="headline" value="<?php echo $article->headline ?>" id="headline" /><br />
+        <textarea name="content" id="content"><?php echo $article->content ?></textarea><br />
+        <div id="formMenu">
+            <input type="checkbox" id="ispage" name="ispage" <?php if ($article->ispage) { echo 'checked'; } ?>>Is Page</input><br />
+            <a href="dashboard.php">Cancle</a>
+            <button type="submit">Submit</button>
+        </div>
+    </form>  
+   
+    <div id="output"></div>        
 
-
+    <script type="text/javascript">
+        $(function() {    
+                    
+            $("#content").resizable({
+                resize: function() {
+                    var top = $(this).css("top");
+                    top = top.substring(0, top.length - 2);
+                    var height = $(this).css("height");
+                    height = height.substring(0, height.length - 2);
+                    var menuTop = parseInt(top) + parseInt(height) + 20;
+                    $("#formMenu").css("top", menuTop + "px");
+                }
+            });
+           
+            $("#headline").css("width", $("#content").css("width"));
+            
+            function updatePreview() {
+                $("#output").html(markdown.toHTML("# " + $("#headline").val() + "\n" + $("#content").val()));
+            }
+            
+            $("#headline").keyup(function() {
+               updatePreview(); 
+            });
+            
+            $("#content").keyup(function() {
+               updatePreview(); 
+            });
+            
+            updatePreview();
+        });
+    </script>
 </body>
 </html>
